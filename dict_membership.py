8e0ea1c97babafe270acf817b47a567ffe2dba7e
pnthairu <37787459+pnthairu@users.noreply.github.com>
2020-06-28 18:42:04 -0500

    Add files via upload

---
+++ dict_membership.py
@@ -0,0 +1,18 @@
+# Start Program
+"""
+Program: dict_membership.py
+Author: Paul Thairu
+Last date modified: 06/27/2020
+
+declare an unimplemented function in_set.
+This will eventually accept a set and return a boolean value stating if the element is in the set.
+First, it contains pass only.
+"""
+
+# declaring an unimplemented function in_set
+def in_dict(in_set, element):
+    if element in in_set:
+        return True
+    return False
+
+# End Program
